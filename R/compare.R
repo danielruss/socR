e96f74b8ea9adad89cc241c8661cac7c840a6089
danielruss <druss@mail.nih.gov>
2022-07-08 15:30:21 -0400

    add codesAgree

---
+++ R/compare.R
@@ -0,0 +1,21 @@
+
+#' Check if codes agree with reviewer
+#'
+#' Compares if a vector of codes is in a vector of reviewer
+#' codes.
+#'
+#' Particularly useful when combined with purrr::map_lgl
+#'
+#' @param codes codes to compare
+#' @param reviewer reviewer's code -- "gold" standard
+#'
+#' @return TRUE if the codes are in the reviewer
+#' @export
+#'
+#' @examples
+#' x <- '11-1011'
+#' codesAgree(x,c("11-1011","11-1021"))
+#' codesAgree(x,c("13-1011","11-1021"))
+codesAgree <- function(codes, reviewer){
+  return(codes %in% reviewer)
+}
